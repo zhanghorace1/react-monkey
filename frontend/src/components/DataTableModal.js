e4ffad426241881b2235d76cadc95f264be326cd
zhanghorace1 <zhanghorace@gmail.com>
2025-05-30 13:58:27 -0400

    added copy button and dummy contact data in dev

--- frontend/src/components/DataTableModal.js
+++ frontend/src/components/DataTableModal.js
@@ -4,11 +4,20 @@ import './DataTableModal.css';
 const DataTableModal = ({ isOpen, onClose, data }) => {
   if (!isOpen) return null;
 
+  const copyToClipboard = () => {
+    const tableData = data.map(item => `${item.firstName}\t${item.lastName}\t${item.email}`).join('\n');
+    navigator.clipboard.writeText(tableData).then(() => {
+      alert('Table data copied to clipboard!');
+    });
+  };
+  
+
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <button className="close-button" onClick={onClose}>X</button>
         <h2>Subscriber Contact Info</h2>
+        <button className="copy-button" onClick={copyToClipboard}>Copy to Clipboard</button>
         <div className="table-wrapper"> {/* Wrapper for vertical scrolling */}
           <table className="data-table">
             <thead>
